9ce218ab32952382347f1b708cc8d4452ac666df
Pranshu Jaiswal (c) <prjaiswal@vmware.com>
2021-11-22 06:42:52 +0000

    Various bug fixes for v1.3.1

--- src/core/vcd/vcdValidations.py
+++ src/core/vcd/vcdValidations.py
@@ -3264,14 +3264,15 @@ class VCDMigrationValidation:
                             nextHopIp = staticRoute['nextHop']
                             if not self.isStaticRouteAutoCreated(edgeGatewayId, nextHopIp):
                                 if not precheck:
-                                    logger.warning(f"Source OrgVDC EdgeGateway {edgeGatewayName} has static routes "
-                                                   f"configured. Please configure equivalent rules directly on "
-                                                   f"external network Tier-0/VRF.\n")
+                                    logger.warning(
+                                        f"Source OrgVDC EdgeGateway {edgeGatewayName} has static routes configured. "
+                                        "These static route will not be migrated. Please configure equivalent rules "
+                                        "directly on external network Tier-0/VRF.\n")
                                 else:
                                     errorList.append(
                                         f"WARNING : Source OrgVDC EdgeGateway {edgeGatewayName} has static routes "
-                                        f"configured. Please configure equivalent rules directly on external "
-                                        f"network Tier-0/VRF.\n")
+                                        "configured. These static route will not be migrated.Please configure "
+                                        "equivalent rules directly on external network Tier-0/VRF.\n")
                                 break
                 except KeyError:
                     logger.debug('Static routes not present in edgeGateway configuration.\n')
