0e21f409045410254680759bd2a909fbe0e73287
sshariqushar <sshariqushar@vmware.com>
2022-03-11 19:59:08 +0530

    UDP custom monitor changes

--- src/core/vcd/vcdValidations.py
+++ src/core/vcd/vcdValidations.py
@@ -3034,9 +3034,15 @@ class VCDMigrationValidation:
                     for pool in listify(responseDict['loadBalancer'].get('pool')):
                         for monitor in listify(responseDict['loadBalancer'].get('monitor')):
                             if pool['monitorId'] == monitor['monitorId']:
-                                if any(key in monitor for key in ['expected', 'send', 'recieve', 'extension']) or \
-                                        (monitor.get('url') and monitor.get('url') != '/'):
-                                    loadBalancerErrorList.append("Custom monitor '{}' used in pool '{}'\n".format(monitor['name'], pool['name']))
+                                if monitor['type'] in ['tcp', 'http', 'https', 'icmp']:
+                                    if any(key in monitor and monitor[key] for key in ['expected', 'send', 'recieve', 'extension']) or \
+                                            (monitor.get('url') and monitor.get('url') != '/'):
+                                        loadBalancerErrorList.append("Custom monitor '{}' used in pool '{}'\n".format(monitor['name'], pool['name']))
+                                elif monitor['type'] == 'udp':
+                                    logger.warning("UDP monitor {} send / receive will be set based on the Avi System-UDP".format(monitor['name']))
+                                    if any(key in monitor and monitor[key] for key in ['extension']) or (monitor.get('url') and monitor.get('url') != '/'):
+                                        loadBalancerErrorList.append("Custom monitor '{}' used in pool '{}'\n".format(monitor['name'], pool['name']))
+
 
                     # url for getting edge gateway load balancer virtual servers configuration
                     url = '{}{}'.format(
